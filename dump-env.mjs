1cbc96847c23e34083c7f535743cb110b8e824de
Alhadis <gardnerjohng@gmail.com>
2020-10-02 16:02:02 +1000

    Add script to dump globals provided by a JS engine

---
+++ dump-env.mjs
@@ -0,0 +1,19 @@
+#!/bin/sh
+//bin/sh -c :; exec node "$0" -- "$@"; # -*- JS -*-
+
+import print from "./print.mjs";
+
+const str = print(globalThis, "globalThis", {
+	all:      true,
+	colours:  true,
+	proto:    true,
+	maxDepth: Infinity,
+});
+
+if("object" === typeof console && console && "function" === typeof console.log)
+	console.log(str);
+
+else if("function" === typeof globalThis.print && globalThis.print !== print)
+	globalThis.print(str);
+
+else throw str;
